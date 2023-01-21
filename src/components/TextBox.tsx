25ee1bdeb90e2d17cfa0a6369b32e210f4319f05
IanConceicao <IanCon234@gmail.com>
2023-01-21 00:15:55 -0800

    Played around with collecting input from a text field

--- src/components/TextBox.tsx
+++ src/components/TextBox.tsx
@@ -1,27 +1,44 @@
-import { Box, Container, TextField } from "@mui/material";
-import * as React from "react";
-import { Component } from "react";
+import { Box, Button, Container, TextField } from "@mui/material";
+import { Component, useState } from "react";
 
 interface TextBoxProps {}
 
 const TextBox: React.FC<TextBoxProps> = () => {
+  const [text, setText] = useState("olá");
+  const [error, setError] = useState(false);
+
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+
+    if (!text) {
+      setError(true);
+    } else {
+      setError(false);
+      console.log(text);
+    }
+  };
   return (
-    <Box
-      component="form"
-      sx={{
-        "& > :not(style)": { width: "100%" },
-      }}
-      noValidate
-      autoComplete="off"
-    >
+    <Box component="form" noValidate autoComplete="off" onSubmit={handleSubmit}>
       <TextField
         id="input-text"
+        onChange={(e) => setText(e.target.value)}
         label="Input Your Portuguese Text Here"
         variant="outlined"
         multiline
         rows={4}
-        defaultValue="Olá"
+        defaultValue={text}
+        fullWidth
+        margin="normal"
+        error={error}
+        helperText={error ? "Enter some text" : ""}
       />
+      <Button
+        variant="contained"
+        type="submit"
+        onClick={() => console.log("you clicked me!")}
+      >
+        Translate
+      </Button>
     </Box>
   );
 };
